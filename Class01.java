3e082786171d0c713c4bbb7b95f9f0da963af5a6
110b13061 <113488308+110b13061@users.noreply.github.com>
2022-12-21 11:24:52 +0800

    Create Class01.java

---
+++ Class01.java
@@ -0,0 +1,11 @@
+package javaQ2;
+
+public class Class01 {
+	public static void main(String args[]) {
+		String str;
+		str="1234";
+			System.out.println("密碼輸入測試");
+			System.out.println("請輸入密碼:"+str);
+		
+	}
+}
